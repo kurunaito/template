89da99e21734974100fbea39e1d79a5a15247bf5
273c <weather@shower.rash.jp>
2014-02-25 11:17:00 +0900

    js option

--- tumblr-theme/js/tagcloud.js
+++ tumblr-theme/js/tagcloud.js
@@ -208,29 +208,3 @@ var TumblrPosts = function(option){
 	};
 
 }(TumblrPosts.prototype, jQuery));
-
-   (function() {
-// インスタンスを生成
-var myTumblrPosts = new TumblrPosts();
-
-// 読み込み完了時のイベントハンドラを設定
-myTumblrPosts.on("complete", function(){
-    var $ul, tags;
-
-    // getTags() : タグリストを取得
-    tags = this.getTags();
-    $ul = $("<ul>");
-
-    $.each(tags, function(i, tag){
-        $ul.append(
-            $("<li>").text(tag.name + " (" + tag.count + ")")
-        );
-    });
-
-    $ul.appendTo( $("#tagcloud") );
-});
-
-// 読み込みを開始
-myTumblrPosts.run();
-
- })();
