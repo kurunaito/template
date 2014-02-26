949f7347bc843381735e615e4bd65b536ad9e0eb
273c <weather@shower.rash.jp>
2014-02-26 22:42:47 +0900

    .page-top

--- tumblr-theme/js/cathedral.js
+++ tumblr-theme/js/cathedral.js
@@ -1,3 +1,18 @@
+$(document).ready(function() {
+	var pagetop = $('.pagetop');
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 700) {
+			pagetop.fadeIn();
+		} else {
+			pagetop.fadeOut();
+		}
+	});
+	pagetop.click(function () {
+		$('body, html').animate({ scrollTop: 0 }, 500);
+		return false;
+	});
+});
+
 /*jshint undef: true */
 /*global jQuery: true */
 /*
